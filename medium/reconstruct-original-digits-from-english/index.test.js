349aa96aa24e81948b2c93f41c82780ae06ea0d6
Hoang Tran <son.hoang01@gmail.com>
2020-11-11 11:56:21 +0800

    pass solution 2

--- medium/reconstruct-original-digits-from-english/index.test.js
+++ medium/reconstruct-original-digits-from-english/index.test.js
@@ -1,17 +1,17 @@
 const test = require('ava');
 const originalDigits = require('.');
-
+const originalDigits2 = require('./solution2');
 // https://leetcode.com/problems/reconstruct-original-digits-from-english/
 
 const DATA = [
-    // {
-    //     input: "owoztneoer",
-    //     output: "012"
-    // },
-    // {
-    //     input: "fviefuro",
-    //     output: "45"
-    // },
+    {
+        input: "owoztneoer",
+        output: "012"
+    },
+    {
+        input: "fviefuro",
+        output: "45"
+    },
     {
         input: "zerozero",
         output: "00"
@@ -19,8 +19,15 @@ const DATA = [
 ];
 
 DATA.forEach( item => {
-    test(`${item.input} should be ${item.output}`, t => {
+    test(`${item.input} should be ${item.output} by using solution 1`, t => {
         const _output = originalDigits(item.input);
+        t.is(_output, item.output);
+    })
+});
+
+DATA.forEach( item => {
+    test(`${item.input} should be ${item.output} by using solution 2`, t => {
+        const _output = originalDigits2(item.input);
         t.is(_output, item.output);
     })
 });
